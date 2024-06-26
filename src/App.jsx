6f98e1760dfe05519304a7498ef5402c3a686401
idan.baker <idan.baker@infinitylabs.co.il>
2024-06-26 15:13:35 +0300

    after phase 1

--- src/App.jsx
+++ src/App.jsx
@@ -5,21 +5,33 @@ import Counter from './Counter';
 import './styles.css';
 
 function App() {
-  const [total, setTotal] = useState(0);
-  const incrementTotal = () => setTotal(currentTotal => currentTotal + 1);
+    const [total, setTotal] = useState(0);
+    const incrementTotal = () => setTotal(currentTotal => currentTotal + 1);
 
-  return (
-    <div className="App">
-      <div>
-        <h4>Total Clicks: {total}</h4>
-      </div>
-      <div className="Container">
-        <Counter onClick={incrementTotal} />
-        <Counter onClick={incrementTotal} />
-        <Counter onClick={incrementTotal} />
-      </div>
-    </div>
-  );
+    const Description = () => {
+        return (
+            <p>
+                This workes prfectly
+                and displays the actual total which is {total}
+            </p>
+
+        )
+
+    }
+
+    return (
+        <div className="App">
+            <div>
+                <h4>Total Clicks: {total}</h4>
+                <Description />
+            </div>
+            <div className="Container">
+                <Counter onClick={incrementTotal} />
+                <Counter onClick={incrementTotal} />
+                <Counter onClick={incrementTotal} />
+            </div>
+        </div>
+    );
 }
 
 export default App;
